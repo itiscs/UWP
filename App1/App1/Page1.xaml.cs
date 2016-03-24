eb75b7adb142a90625e2137e0e4a0a4eb164cf0d
moudroff <moudroff>
2016-03-24 11:43:08 +0300

    App State

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+
+        public Page1()
+        {
+            this.InitializeComponent();
+            LoadState();
+        }
+
+        internal void SaveState()
+        {
+            ApplicationData.Current.RoamingSettings.Values["Title"] = txtTitle.Text;
+            ApplicationData.Current.RoamingSettings.Values["bookPage"] =
+                book.SelectedIndex;
+        }
+
+        internal void LoadState()
+        {
+            var rs = ApplicationData.Current.RoamingSettings;
+            if (rs.Values["Title"] != null)
+                txtTitle.Text = rs.Values["Title"].ToString();
+            if (rs.Values["bookPage"] != null)
+                book.SelectedIndex = Convert.ToInt32(rs.Values["bookPage"]);
+        }
+    }
+}
